f68adee5db2aab18c7e653ad914f7b3e46119f0c
zhe-qi <zheqi0910@gmail.com>
2023-02-07 23:29:18 +0800

    bug fix

--- components/BG.vue
+++ components/BG.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 const { isDark } = useTheme()
 
+const bg = ref<HTMLElement | null>(null)
+// TODO: Fix this, 刷新样式丢失，所以这样补救
+onMounted(() => {
+  if (isDark.value) { bg.value?.classList.remove('bg') }
+})
 </script>
 
 <template>
-  <div :class="isDark ? '' : 'bg' " />
+  <div ref="bg" :class="isDark ? '' : 'bg' " />
 </template>
 
 <style scoped lang="postcss">
--- components/HeaderIcon/Switch.vue
+++ components/HeaderIcon/Switch.vue
@@ -2,18 +2,17 @@
 import { Switch } from '@headlessui/vue'
 
 const { toggleDark, isDark } = useTheme()
+
 </script>
 
 <template>
   <Switch
-    :class="isDark ? 'bg-zinc-700' : 'bg-zinc-200'"
-    class="relative inline-flex h-[29px] w-[54px] items-center shrink-0 cursor-pointer rounded-full border-2 border-gray-300 transition-colors duration-200 ease-in-out"
+    class="dark:bg-zinc-700 bg-zinc-200 relative inline-flex h-[29px] w-[54px] items-center shrink-0 cursor-pointer rounded-full border-2 border-gray-300 transition-colors duration-200 ease-in-out"
     @click="toggleDark()"
   >
     <div
       aria-hidden="true"
-      :class="isDark ? 'translate-x-[1.6rem]' : 'translate-x-[0.1rem]'"
-      class="pointer-events-none inline-flex justify-center items-center h-[23px] w-[23px] transform rounded-full bg-white shadow-lg text-black ring-0 transition duration-200 ease-in-out"
+      class=" dark:translate-x-[1.6rem] translate-x-[0.1rem] pointer-events-none inline-flex justify-center items-center h-[23px] w-[23px] transform rounded-full bg-white shadow-lg text-black ring-0 transition duration-200 ease-in-out"
     >
       <Icon v-if="isDark" name="material-symbols:dark-mode-outline" />
       <Icon v-else name="material-symbols:sunny-outline" />
